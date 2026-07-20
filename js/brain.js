2018b388f6eed91fabeb1672933699eeabe9fae3
justinioh <justinmuraya2@gmail.com>
2026-07-20 12:11:51 +0300

    first commit

---
+++ js/brain.js
@@ -0,0 +1,20 @@
+
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-btn");
+const drawer = document.getElementById("drawer");
+const overlay = document.getElementById("overlay");
+
+menuBtn.addEventListener("click", () => {
+    drawer.classList.add("open");
+    overlay.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+    drawer.classList.remove("open");
+    overlay.classList.remove("show");
+});
+
+overlay.addEventListener("click", () => {
+    drawer.classList.remove("open");
+    overlay.classList.remove("show");
+});
